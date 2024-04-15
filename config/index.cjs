eca5bb3f9d055d864cf7fc12e548befa8c9db3af
Zxx0522 <111743869+Zxx0522@users.noreply.github.com>
2024-04-15 21:45:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '姐姐', year: '2024', date: '01-17',
+          type: '生日', name: '姐姐', year: '2001', date: '01-12',
         },
         {
           type: '节日', name: '相识纪念日', year: '2024', date: '03-15',
